dd6b57b25acce3e4dea94205784c798d5dae9c3b
fireapache <vinicius-rs@hotmail.com.br>
2013-12-29 19:31:58 -0200

    Scripts iniciais

---
+++ Controllers/CombatController.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var anim : Animator;
+
+function Start()
+{
+	anim = gameObject.GetComponent(Animator);
+}
+
+function Update()
+{
+	if (Input.GetButtonDown("Fire1"))
+	{
+		anim.SetBool("punch", true);
+	}
+	else anim.SetBool("punch", false);
+}
---
+++ Controllers/MovementController.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var speed : float = 5;
+var direction : float = 0;
+var anim : Animator;
+
+function Start()
+{
+	anim = gameObject.GetComponent(Animator);
+}
+
+function Update()
+{
+	direction = Input.GetAxis("Horizontal") * Time.deltaTime;
+	
+	transform.position.x += direction;
+	
+	anim.SetFloat("direction", direction);
+	
+	anim.SetFloat("speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+}
