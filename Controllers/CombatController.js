afbd18941b88a406780607b88602af5242b70a65
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2014-01-11 21:25:23 -0200

    Adaptação para física 3D

--- Controllers/CombatController.js
+++ Controllers/CombatController.js
@@ -40,56 +40,54 @@ function setInitialHitSignals()
 {
 	anim.SetBool("punch", false);
 	anim.SetBool("upperKick", false);
-	
-	lastStateNameHash = 0;
 }
 
 // Funçao que trata as colisoes e aplica o golpe adequado.
-function collisionDetection(collision : Collision2D)
+function collisionDetection(collision : Collision)
 {
 	// Se nao estiver num estado de detecçao de colisao, entao retorna.
 	if (anim.GetCurrentAnimatorStateInfo(0).tagHash != hitTagHash)
 	{
-		// Volta para o estado inicial.
-		setInitialHitSignals();
-		
+		lastStateNameHash = 0;
 		return;
-	}
-	
-	// Caso esteja duplicando o golpe num mesmo estado, retorna.
-	if (anim.GetCurrentAnimatorStateInfo(0).nameHash == lastStateNameHash)
-	{
-		return;
-	}
-	else
-	{
-		lastStateNameHash = anim.GetCurrentAnimatorStateInfo(0).nameHash;
 	}
 	
 	// Caso o objeto impactado seja de tag Player, entao o golpe e aplicado.
 	if (collision.gameObject.tag == playerTag)
 	{
+		// Caso esteja duplicando o golpe num mesmo estado, retorna.
+		if (anim.GetCurrentAnimatorStateInfo(0).nameHash == lastStateNameHash)
+		{
+			return;
+		}
+		else
+		{
+			lastStateNameHash = anim.GetCurrentAnimatorStateInfo(0).nameHash;
+		}
+		
 		Debug.Log("Pow " + lastStateNameHash);
 	}
 }
 
-// Funçao de tratamento de colisao.
-function OnCollisionStay2D(collisionInfo : Collision2D)
-{
-	collisionDetection(collisionInfo);
-}
-
 // Funçao de tratamento de inicio de colisao.
-function OnCollisionEnter2D(collisionInfo : Collision2D)
+function OnCollisionEnter(collisionInfo : Collision)
 {
 	collisionDetection(collisionInfo);
 }
 
+// Funçao de tratamento de colisao.
+function OnCollisionStay(collisionInfo : Collision)
+{
+	collisionDetection(collisionInfo);
+}
 
 // Procedimento executado a cada loop do jogo. Aqui sao feitos o tratamento de 
 //entradas e gerenciamento das animaçoes de golpes.
 function Update()
 {
+	// Volta para o estado inicial.
+	setInitialHitSignals();
+
 	// Teste temporario para procedimento de golpe generico.
 	if (Input.GetButtonDown("Fire1"))
 	{
--- Controllers/MovementController.js
+++ Controllers/MovementController.js
@@ -12,11 +12,44 @@ var speed : float = 5;
 var direction : float = 0;
 // Referencia ao Animator com as animaçoes dos golpes e movimentos do personagem.
 var anim : Animator;
+// Tag que simboliza o chao.
+var floorTag : String;
+// Referencia ao controlador de fisica.
+var rigBody : Rigidbody;
 
 // Configuraçao inicial.
 function Start()
 {
+	// Adiquire as referencia do Animator e do RigidBody.
 	anim = gameObject.GetComponent(Animator);
+	rigBody = gameObject.GetComponent(Rigidbody);
+	
+	// Limita a fisica do personagem para o 2D.
+	rigBody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+	
+	// O personagem começa caindo, como se tivesse pulado.
+	anim.SetBool("jumping", true);
+	
+	floorTag = "Floor";
+}
+
+// Funçao de tratamento de inicio de colisao.
+function OnCollisionEnter(collisionInfo : Collision)
+{
+	if (collisionInfo.gameObject.tag != floorTag)
+	{
+		return;
+	}
+	else if (anim.GetBool("jumping") == true)
+	{
+		anim.SetBool("jumping", false);
+	}
+}
+
+// Funçao de tratamento de colisao.
+function OnCollisionStay(collisionInfo : Collision)
+{
+	
 }
 
 // Procedimento executado a cada loop do jogo. Aqui sao feitos o tratamento de 
